556089ea3f0d900c9267690ee0c07302ac8f13c9
kk795896108-rgb <kk795896108@gmail.com>
2026-03-09 20:50:15 +0530

    bank.html file added

---
+++ bank.js
@@ -0,0 +1,38 @@
+let bankbalance=5000;
+let withdraw=0;
+let deposite=0;
+function DEPOSIT(){
+    let amount =Number(document.getElementById("amount").value);
+    if(amount>0){
+        deposite+=amount;
+        bankbalance+=amount;
+        document.getElementById("deposite").innerHTML=deposite;
+        document.getElementById("deposite").style.color="green";
+        document.getElementById("withdraw").style.color="black";
+        document.getElementById("balance").innerHTML=bankbalance;
+        document.getElementById("balance").style.color="green";
+        document.getElementById("bankbalance").innerHTML=bankbalance;
+        document.getElementById("message").innerHTML="congracts! Your Amount $"+amount+"is succesfully deposited.";
+        document.getElementById("message").style.color="green";
+    
+    }
+}
+function WITHDRAW(){
+    let amount=Number(document.getElementById("amount").value);
+    if(amount<=bankbalance){
+        withdraw+=amount;
+        bankbalance-=amount;
+           document.getElementById("withdraw").innerHTML=withdraw;
+           document.getElementById("withdraw").style.color="red";
+           document.getElementById("deposite").style.color="black"
+        document.getElementById("balance").innerHTML=bankbalance;
+        document.getElementById("balance").style.color="red";
+        document.getElementById("bankbalance").innerHTML=bankbalance;
+        document.getElementById("message").innerHTML="Your Amount $"+amount+"is withdrawn succesfully .";
+        document.getElementById("message").style.color="red";
+    }
+    else{
+        document.getElementById("message").innerText="Insufficient bank balance.";
+        document.getElementById("message").style.color="purple";
+    }
+}
